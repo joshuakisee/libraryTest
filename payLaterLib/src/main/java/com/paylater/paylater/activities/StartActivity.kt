0486ac4ab29eb05b44832b28c4f7602fca991fb0
joshua kisee <kiseej@gmail.com>
2022-01-10 14:30:57 +0300

    fixing firebase

--- payLaterLib/src/main/java/com/paylater/paylater/activities/StartActivity.kt
+++ payLaterLib/src/main/java/com/paylater/paylater/activities/StartActivity.kt
@@ -68,11 +68,17 @@ class StartActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItem
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
-        var clientName = intent.getStringExtra("fullName")
-        var clientPhone = intent.getStringExtra("phoneNumber")
-        var clientScore = intent.getStringExtra("score")
-        var publicKey = intent.getStringExtra("publicKey")
-        var privateKey = intent.getStringExtra("privateKey")
+//        var clientName = intent.getStringExtra("fullName")
+//        var clientPhone = intent.getStringExtra("phoneNumber")
+//        var clientScore = intent.getStringExtra("score")
+//        var publicKey = intent.getStringExtra("publicKey")
+//        var privateKey = intent.getStringExtra("privateKey")
+
+        var clientName = "joshua"
+        var clientPhone = "0705118708"
+        var clientScore = "700"
+        var publicKey = "O4bmMWglsFGxbDSOHNx2zL2C2H2tC1Dw"
+        var privateKey = "YTLKaH25wtt4zCj1"
 
         if (clientName != null) {
             if (clientName.isEmpty()){
@@ -147,18 +153,17 @@ class StartActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItem
         val filter: LinearLayout = findViewById(R.id.filter)
         val filterNow: LinearLayout = findViewById(R.id.filter_now)
 
-        FirebaseDatabase.getInstance();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
 
-        if(!FirebaseApp.getApps(this).isEmpty()) {
-            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+//        if(FirebaseApp.getApps(this).isNotEmpty()) {
+//            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+//
+//            Toast.makeText(this, "firebase was not initialized", Toast.LENGTH_LONG).show()
+//            Log.d("firebaseTokenNot", "firebase was not initialized")
+//
+//        }
 
-            Toast.makeText(this, "firebase was not initialized", Toast.LENGTH_LONG).show()
-            Log.d("firebaseTokenNot", "firebase was not initialized")
 
-        }
-
-
-//        FirebaseApp.initializeApp(this);
 //        FirebaseApp.initializeApp(this)
 //        if(FirebaseApp.getApps(this).isEmpty()){
 //            FirebaseApp.initializeApp(this)
